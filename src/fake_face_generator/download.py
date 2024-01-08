fe1f0b1fcb643ac39987f2fdf0d7d9025e4e754f
plachert <pmlachert@gmail.com>
2024-01-08 17:21:43 +0100

    Add docstrings, refactor

--- src/fake_face_generator/download.py
+++ src/fake_face_generator/download.py
@@ -9,6 +9,7 @@ import backoff
 from tqdm.asyncio import tqdm_asyncio
 
 URL = "https://thispersondoesnotexist.com"
+CONCURRENT_LIMIT = 50
 
 
 class DuplicatedImageError(Exception):
@@ -16,18 +17,42 @@ class DuplicatedImageError(Exception):
 
 
 def hash_bytes(bytes):
+    """
+    Generate an MD5 hash for the given bytes.
+
+    Args:
+        bytes: The input bytes to be hashed.
+
+    Returns:
+        str: The hexadecimal representation of the MD5 hash.
+    """
     hash = hashlib.md5()
     hash.update(bytes)
     return hash.hexdigest()
 
 
-@backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=3)
+@backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=10)
 @backoff.on_exception(backoff.expo, DuplicatedImageError, max_tries=None)
 async def download_image(
     session: aiohttp.ClientSession,
     seen: set,
     data_dir: pathlib.Path,
 ):
+    """
+    Download an image and save it if it's unique.
+
+    Args:
+        session (aiohttp.ClientSession): The aiohttp session for making HTTP requests.
+        seen (set): A set containing hashes of previously downloaded images.
+        data_dir (pathlib.Path): The directory where images will be saved.
+
+    Raises:
+        aiohttp.ClientResponseError: If the HTTP response status code is not in the 2xx range.
+        DuplicatedImageError: If the downloaded image is a duplicate.
+
+    Returns:
+        None
+    """
     async with session.get(url=URL) as response:
         if 200 <= response.status < 300:
             content = await response.read()
@@ -47,14 +72,42 @@ async def download_image(
 
 
 async def save_image(bytes, filename):
+    """
+    Save the given bytes to a file.
+
+    Args:
+        bytes: The bytes to be saved.
+        filename (pathlib.Path): The path where the file will be saved.
+
+    Returns:
+        None
+    """
     async with aiofiles.open(filename, "wb") as file:
         await file.write(bytes)
 
 
 async def download_all_images(data_dir: pathlib.Path, n_images: int):
+    """
+    Download a specified number of unique images concurrently.
+
+    Args:
+        data_dir (pathlib.Path): The directory where images will be saved.
+        n_images (int): The number of unique images to be generated.
+
+    Returns:
+        None
+    """
     seen = set()
-    async with aiohttp.ClientSession() as session:
-        tasks = [download_image(session, seen, data_dir) for _ in range(n_images)]
+    semaphore = asyncio.Semaphore(CONCURRENT_LIMIT)
+    async with aiohttp.ClientSession(
+        timeout=aiohttp.ClientTimeout(total=60)
+    ) as session:
+
+        async def download_with_semaphore():
+            async with semaphore:
+                await download_image(session, seen, data_dir)
+
+        tasks = [download_with_semaphore() for _ in range(n_images)]
         await tqdm_asyncio.gather(*tasks, desc="Downloading fake images")
 
 
