cce3486e40c2f6215e250b8ca8356258ab861c5a
plachert <pmlachert@gmail.com>
2024-01-08 15:24:03 +0100

    Separate backoff policies, refactor

--- src/fake_face_generator/download.py
+++ src/fake_face_generator/download.py
@@ -1,6 +1,5 @@
 import asyncio
 import hashlib
-import uuid
 
 import aiofiles
 import aiohttp
@@ -19,39 +18,41 @@ def hash_bytes(bytes):
     return hash.hexdigest()
 
 
-@backoff.on_exception(
-    backoff.expo, (aiohttp.ClientError, DuplicatedImageError), max_tries=None
-)
-async def fetch_bytes(session, seen: set):
-    try:
-        async with session.get(url=URL) as response:
-            if response.status == 200:
-                content = await response.read()
-                hashed = hash_bytes(content)
-                if hashed in seen:
-                    raise DuplicatedImageError
-                filename = f"data/{uuid.uuid1()}.jpg"
-                async with aiofiles.open(filename, "wb") as file:
-                    await file.write(content)
-                seen.add(hashed)
-            else:
-                print(f"Error: {response.status}")
-    except aiohttp.ClientError as e:
-        print(f"Aiohttp error: {e}")
-        raise
+@backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=3)
+@backoff.on_exception(backoff.expo, DuplicatedImageError, max_tries=None)
+async def download_image(session, seen: set):
+    async with session.get(url=URL) as response:
+        if 200 <= response.status < 300:
+            content = await response.read()
+            hashed = hash_bytes(content)
+            if hashed in seen:
+                raise DuplicatedImageError
+            filename = f"data/{len(seen)}.jpg"
+            await save_image(content, filename)
+            seen.add(hashed)
+        else:
+            raise aiohttp.ClientResponseError(
+                response.request_info,
+                response.history,
+                code=response.status,
+                message=response.reason,
+            )
 
 
-async def fetch_all():
+async def save_image(bytes, filename):
+    async with aiofiles.open(filename, "wb") as file:
+        await file.write(bytes)
+
+
+async def download_all():
     seen = set()
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch_bytes(session, seen) for _ in range(10)]
-        byte_responses = await asyncio.gather(*tasks)
-
-        return byte_responses
+        tasks = [download_image(session, seen) for _ in range(10)]
+        await asyncio.gather(*tasks)
 
 
 async def main():
-    await fetch_all()
+    await download_all()
 
 
 if __name__ == "__main__":
