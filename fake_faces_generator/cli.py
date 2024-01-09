88593090f4202e04e1aeca466a5a50ed6b670a30
plachert <pmlachert@gmail.com>
2024-01-09 11:14:48 +0100

    Fix precommit

--- fake_faces_generator/cli.py
+++ fake_faces_generator/cli.py
@@ -6,14 +6,14 @@ from download import run
 
 @click.command()
 @click.argument(
-        "data_dir",
-        type=click.Path(
-            exists=False,
-            file_okay=False,
-            dir_okay=True,
-        ),
-        required=True,
-        )
+    "data_dir",
+    type=click.Path(
+        exists=False,
+        file_okay=False,
+        dir_okay=True,
+    ),
+    required=True,
+)
 @click.argument("n_images", type=int, required=True)
 def cli(data_dir, n_images):
     """
@@ -25,12 +25,13 @@ def cli(data_dir, n_images):
         N_IMAGES (int): Number of unique images to be generated (must be greater than 0).
 
     Examples:
-    - To download 10 images to the 'images' directory: 
-    
+    - To download 10 images to the 'images' directory:
+
     $ fake_faces_generator /path/to/images 10
     """
     data_dir = pathlib.Path(data_dir)
     run(data_dir, n_images)
+
 
 if __name__ == "__main__":
     cli()
