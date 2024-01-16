9d86f86423d26307592a3440c99d429fe02519b9
plachert <pmlachert@gmail.com>
2024-01-16 17:09:20 +0100

    Test failing download

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,13 +1,60 @@
 import pathlib
+import pickle
+import shutil
+from unittest.mock import patch
+
+import pytest
 
 import fake_faces_generator.download as download
 
 
-def test_successful_download(tmpdir):
+class SimulatedException(Exception):
+    pass
+
+
+class FailingSet(set):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def add(self, element):
+        if len(self) < 2:
+            super().add(element)
+        else:
+            raise SimulatedException
+
+
+@pytest.fixture
+def cleanup(tmpdir):
     tmpdir = pathlib.Path(tmpdir)
-    download.run(tmpdir, 3)
-    jpg_files = list(tmpdir.glob("*.jpg"))
-    pkl_files = list(tmpdir.glob("*.pkl"))
+    yield tmpdir
+    shutil.rmtree(tmpdir)
+
+
+@patch("fake_faces_generator.download.set")
+def test_failing_download(mocked_set, cleanup):
+    mocked_set.side_effect = lambda: FailingSet()
+    try:
+        download.run(cleanup, 3)
+    except SimulatedException:
+        pkl_files = list(cleanup.glob("*.pkl"))
+        assert len(pkl_files) == 1
+        assert pkl_files[0].name == "seen_images.pkl"
+
+
+def test_successful_download(cleanup):
+    download.run(cleanup, 3)
+    jpg_files = list(cleanup.glob("*.jpg"))
+    pkl_files = list(cleanup.glob("*.pkl"))
     assert len(jpg_files) == 3
     assert len(pkl_files) == 1
     assert pkl_files[0].name == "seen_images.pkl"
+
+
+def test_incremental_download(cleanup):
+    download.run(cleanup, 3)
+    download.run(cleanup, 3)
+    jpg_files = list(cleanup.glob("*.jpg"))
+    assert len(jpg_files) == 6
+    with open(cleanup / "seen_images.pkl", "rb") as f:
+        seen = pickle.load(f)
+    assert len(seen) == 6
