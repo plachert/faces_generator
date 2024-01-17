493e862f517af0d1c6db2c01677d3e1e3d3a4c1e
plachert <pmlachert@gmail.com>
2024-01-17 10:01:26 +0100

    Refactor

--- tests/test_download.py
+++ tests/test_download.py
@@ -24,37 +24,37 @@ class FailingSet(set):
 
 
 @pytest.fixture
-def cleanup(tmpdir):
+def tmpdir_with_cleanup(tmpdir):
     tmpdir = pathlib.Path(tmpdir)
     yield tmpdir
     shutil.rmtree(tmpdir)
 
 
 @patch("fake_faces_generator.download.set")
-def test_failing_download(mocked_set, cleanup):
+def test_failing_download(mocked_set, tmpdir_with_cleanup):
     mocked_set.side_effect = lambda: FailingSet()
     try:
-        download.run(cleanup, 3)
+        download.run(tmpdir_with_cleanup, 3)
     except SimulatedException:
-        pkl_files = list(cleanup.glob("*.pkl"))
+        pkl_files = list(tmpdir_with_cleanup.glob("*.pkl"))
         assert len(pkl_files) == 1
         assert pkl_files[0].name == "seen_images.pkl"
 
 
-def test_successful_download(cleanup):
-    download.run(cleanup, 3)
-    jpg_files = list(cleanup.glob("*.jpg"))
-    pkl_files = list(cleanup.glob("*.pkl"))
+def test_successful_download(tmpdir_with_cleanup):
+    download.run(tmpdir_with_cleanup, 3)
+    jpg_files = list(tmpdir_with_cleanup.glob("*.jpg"))
+    pkl_files = list(tmpdir_with_cleanup.glob("*.pkl"))
     assert len(jpg_files) == 3
     assert len(pkl_files) == 1
     assert pkl_files[0].name == "seen_images.pkl"
 
 
-def test_incremental_download(cleanup):
-    download.run(cleanup, 3)
-    download.run(cleanup, 3)
-    jpg_files = list(cleanup.glob("*.jpg"))
+def test_incremental_download(tmpdir_with_cleanup):
+    download.run(tmpdir_with_cleanup, 3)
+    download.run(tmpdir_with_cleanup, 3)
+    jpg_files = list(tmpdir_with_cleanup.glob("*.jpg"))
     assert len(jpg_files) == 6
-    with open(cleanup / "seen_images.pkl", "rb") as f:
+    with open(tmpdir_with_cleanup / "seen_images.pkl", "rb") as f:
         seen = pickle.load(f)
     assert len(seen) == 6
